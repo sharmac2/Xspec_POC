1d0f7a9ae591d09373ae63ecb34633c55b054798
sharmac2 <chaitanya.sharma@lexisnexis.com>
2018-05-04 12:43:41 +0530

    Signed-off-by: sharmac2 <chaitanya.sharma@lexisnexis.com>

---
+++ Xspec_test/AU01.bat
@@ -0,0 +1,2 @@
+set SAXON_CP=C:\saxon\saxon9he.jar
+bin\xspec.bat -j Xspec_test\AU01_Rosetta_CASEDOC-to-LxAdv_courtcase.xspec
---
+++ Xspec_test/MY01.bat
@@ -0,0 +1,2 @@
+set SAXON_CP=C:\saxon\saxon9he.jar
+bin\xspec.bat -j Xspec_test\MY01.xspec
